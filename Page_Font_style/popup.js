51614e7321970b86e09380732aacb53f9bb1a54d
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-06-23 23:19:17 +0430

    Save color to chrome storage

--- Page_Font_style/popup.js
+++ Page_Font_style/popup.js
@@ -1,8 +1,13 @@
 const colorInput = document.querySelector("input[type=color]");
 
+chrome.storage.sync.get("color", ({ color }) => {
+  if (color) colorInput.value = color;
+});
+
 colorInput.addEventListener("change", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, { task: "changeColor", color: colorInput.value });
+    chrome.storage.sync.set({ color: colorInput.value });
     close();
   });
 });
