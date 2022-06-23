a344668c84f8fa5f7b49932cfc34b2a7ec549826
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-06-23 14:56:15 +0430

    Options Functionality

---
+++ Budget_Manager/options.js
@@ -0,0 +1,16 @@
+const formEl = document.querySelector("form");
+const limitInput = document.querySelector("input#limit");
+
+chrome.storage.sync.get("limit", ({ limit }) => {
+  limitInput.value = limit || 0;
+});
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (limitInput.value) chrome.storage.sync.set({ limit: +limitInput.value });
+});
+
+formEl.addEventListener("reset", (event) => {
+  event.preventDefault();
+  chrome.storage.sync.set({ total: 0 });
+});
