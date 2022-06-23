84468d816947dfd2d3168fe593db0790868e5cdd
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-06-23 15:01:00 +0430

    Close Tab after job

--- Budget_Manager/options.js
+++ Budget_Manager/options.js
@@ -8,9 +8,11 @@ chrome.storage.sync.get("limit", ({ limit }) => {
 formEl.addEventListener("submit", (event) => {
   event.preventDefault();
   if (limitInput.value) chrome.storage.sync.set({ limit: +limitInput.value });
+  close();
 });
 
 formEl.addEventListener("reset", (event) => {
   event.preventDefault();
   chrome.storage.sync.set({ total: 0 });
+  close();
 });
